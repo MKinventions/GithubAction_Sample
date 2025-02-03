1456c71fd0cfe458885943511e5ab6838e6acd93
Madhan Chiruguri <mchiruguri@semtech.com>
2025-02-03 17:48:59 +0530

    10th commit updated the yml file

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 // Conditional compilation to define the LED pin
 #ifdef ARDUINO_AVR_NANO  // For Arduino Nano (ATmega328)
   #define LED 13  // Built-in LED on pin 13
-#elif defined(ESP8266)   // For ESP8266 (ESP12E)
+#elif defined(esp12e)   // For ESP8266 (ESP12E)
   #define LED LED_BUILTIN  // Built-in LED on GPIO2 (NodeMCU)
 #else
   #define LED 13  // Default to pin 13 if the platform is unknown
