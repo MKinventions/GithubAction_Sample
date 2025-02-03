4ba0f1998ff9c70708e9037d2d9ec095926a8b1c
Madhan Chiruguri <mchiruguri@semtech.com>
2025-02-03 15:48:06 +0530

    6th commit updated the main file and yml file for only arduino nano

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,6 @@
 #include <Arduino.h>
 
-// #define ESP8266
-// #define ARDUINO_NANO
-#define ARDUINO_UNO
-
-#if defined(ESP8266)
-#define LED 16
-#elif defined(ARDUINO_NANO) | defined(ARDUINO_UNO)
 #define LED 13
-#else
-#error "Please Select Proper MCU."
-#endif
-
 
 void setup() {
 Serial.begin(9600);
