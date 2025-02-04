97db7e36a644f7c442d1dda3f9cfc7cd3ec609cd
Madhan Chiruguri <mchiruguri@semtech.com>
2025-02-04 16:24:23 +0530

    13th commit updated the yml and main file with uno

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 #include <Arduino.h>
+#include <EEPROM.h>
 
 // Conditional compilation to define the LED pin
 #ifdef ARDUINO_AVR_NANO  // For Arduino Nano (ATmega328)
+  #define LED 13  // Built-in LED on pin 13
+#elif defined(ARDUINO_AVR_UNO)  // For Arduino Uno (ATmega328)
   #define LED 13  // Built-in LED on pin 13
 #elif defined(esp12e)   // For ESP8266 (ESP12E)
   #define LED LED_BUILTIN  // Built-in LED on GPIO2 (NodeMCU)
@@ -12,17 +15,33 @@
 #endif
 
 
+const int addr1 = 10;
+bool ledState = false;
+
 void setup() {
+  #if defined(ESP32)
+  EEPROM.begin(512);
+  #endif
+
   Serial.begin(9600);
   Serial.println("Restarted");
 
   pinMode(LED, OUTPUT);  // Set LED pin as output
+
+  ledState = EEPROM.read(addr1);
+  Serial.println("LedState:" + String(ledState));
 }
 
-void loop() {
-  static int ledState = 0;
 
+void loop() {
+  
   ledState = !ledState;
+  EEPROM.write(addr1, ledState);
+  #if defined(ESP32)
+  EEPROM.commit();
+  #endif
+
   digitalWrite(LED, ledState);  // Toggle the LED
   delay(1000);  // Wait for 1 second
+
 }
