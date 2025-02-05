c83951ef875ca5926e4eda87cf89af0c6873ef47
Madhan Chiruguri <mchiruguri@semtech.com>
2025-02-05 10:41:06 +0530

    17th commit, added the button for controlling led

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,19 @@
 // Conditional compilation to define the LED pin
 #ifdef ARDUINO_AVR_NANO        // For Arduino Nano (ATmega328)
 #define LED 13                 // Built-in LED on pin 13
+#define BTN 8
 #elif defined(ARDUINO_AVR_UNO) // For Arduino Uno (ATmega328)
-#define LED 13                 // Built-in LED on pin 13
+#define LED 13
+#define BTN 8                 // Built-in LED on pin 13
 #elif defined(esp12e)          // For ESP8266 (ESP12E)
 #define LED LED_BUILTIN        // Built-in LED on GPIO2 (NodeMCU)
+#define BTN 14
 #elif defined(ESP32)           // For ESP32 (DevKit V1, WROOM, etc.)
-#define LED 2                  // Built-in LED on GPIO2
+#define LED 2
+#define BTN 26                  // Built-in LED on GPIO2
 #else
 #define LED 13 // Default to pin 13 if the platform is unknown
+#define BTN 8
 #endif
 
 const int addr1 = 10;
@@ -29,6 +34,7 @@ void setup()
   Serial.println("Restarted");
 
   pinMode(LED, OUTPUT); // Set LED pin as output
+  pinMode(BTN, INPUT_PULLUP);
 
   ledState = EEPROM.read(addr1);
   Serial.println("LedState:" + String(ledState));
@@ -36,8 +42,16 @@ void setup()
 
 void loop()
 {
+  bool buttonState = digitalRead(BTN);
 
-  ledBlink(100);
+  if (buttonState)
+  {
+    digitalWrite(LED, HIGH);
+  }else{
+    digitalWrite(LED, LOW);
+  }
+  
+  // ledBlink(100);
   
 }
 
