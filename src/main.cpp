ce38a0e27ea95dee75c3263f25afe30141cc4e7c
Madhan Chiruguri <mchiruguri@semtech.com>
2025-02-05 10:02:49 +0530

    15th commit, updated the led blinking by adding function

--- src/main.cpp
+++ src/main.cpp
@@ -2,46 +2,52 @@
 #include <EEPROM.h>
 
 // Conditional compilation to define the LED pin
-#ifdef ARDUINO_AVR_NANO  // For Arduino Nano (ATmega328)
-  #define LED 13  // Built-in LED on pin 13
-#elif defined(ARDUINO_AVR_UNO)  // For Arduino Uno (ATmega328)
-  #define LED 13  // Built-in LED on pin 13
-#elif defined(esp12e)   // For ESP8266 (ESP12E)
-  #define LED LED_BUILTIN  // Built-in LED on GPIO2 (NodeMCU)
-#elif defined(ESP32)  // For ESP32 (DevKit V1, WROOM, etc.)
-  #define LED 2  // Built-in LED on GPIO2
+#ifdef ARDUINO_AVR_NANO        // For Arduino Nano (ATmega328)
+#define LED 13                 // Built-in LED on pin 13
+#elif defined(ARDUINO_AVR_UNO) // For Arduino Uno (ATmega328)
+#define LED 13                 // Built-in LED on pin 13
+#elif defined(esp12e)          // For ESP8266 (ESP12E)
+#define LED LED_BUILTIN        // Built-in LED on GPIO2 (NodeMCU)
+#elif defined(ESP32)           // For ESP32 (DevKit V1, WROOM, etc.)
+#define LED 2                  // Built-in LED on GPIO2
 #else
-  #define LED 13  // Default to pin 13 if the platform is unknown
+#define LED 13 // Default to pin 13 if the platform is unknown
 #endif
-
 
 const int addr1 = 10;
 bool ledState = false;
 
-void setup() {
-  #if defined(ESP32)
+void ledBlink();
+
+void setup()
+{
+#if defined(ESP32)
   EEPROM.begin(512);
-  #endif
+#endif
 
   Serial.begin(9600);
   Serial.println("Restarted");
 
-  pinMode(LED, OUTPUT);  // Set LED pin as output
+  pinMode(LED, OUTPUT); // Set LED pin as output
 
   ledState = EEPROM.read(addr1);
   Serial.println("LedState:" + String(ledState));
 }
 
+void loop()
+{
 
-void loop() {
-  
+  ledBlink();
+}
+
+void ledBlink()
+{
   ledState = !ledState;
   EEPROM.write(addr1, ledState);
-  #if defined(ESP32)
+#if defined(ESP32)
   EEPROM.commit();
-  #endif
+#endif
 
-  digitalWrite(LED, ledState);  // Toggle the LED
-  delay(1000);  // Wait for 1 second
-
+  digitalWrite(LED, ledState); // Toggle the LED
+  delay(1000);                 // Wait for 1 second
 }
